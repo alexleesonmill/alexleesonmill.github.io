8b6a4053f2f48591000b7ccf5c13c726664ab267
{{your_name}} <{{your_email}}>
2026-01-25 08:51:40 +0000

    Alter base for vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Update this to match your GitHub repository name, or remove for custom domain
-  base: '/Jake/',
+  base: 'jacobbloomfield',
 });
